d631192155b35d0f062ceb5cc204c77e46b4e993
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-09-27 15:19:51 +0200

    Initial commit

---
+++ src/main/java/intra/poleemploi/PortailServiceApplication.java
@@ -0,0 +1,13 @@
+package intra.poleemploi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortailServiceApplication.class, args);
+	}
+
+}
