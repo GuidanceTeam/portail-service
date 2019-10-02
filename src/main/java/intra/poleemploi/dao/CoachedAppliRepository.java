b57172165d93c31bd9cb3e9e2ad31527efe8623f
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-02 15:04:06 +0200

    [DAO] CoachedAppliRepository & ContentAppliRepository

--- src/main/java/intra/poleemploi/dao/CoachedAppliRepository.java
+++ src/main/java/intra/poleemploi/dao/CoachedAppliRepository.java
@@ -4,7 +4,10 @@ import intra.poleemploi.entities.CoachedAppli;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
-public interface CoachedAppliRepository extends JpaRepository<CoachedAppli, String> {
-    public CoachedAppli findCoachedAppliByAppliName(String appliName);
+public interface CoachedAppliRepository extends JpaRepository<CoachedAppli, Integer> {
+    //public CoachedAppli findCoachedAppliByAppliName(String appliName);
+
 }
--- src/main/java/intra/poleemploi/dao/ContentAppliRepository.java
+++ src/main/java/intra/poleemploi/dao/ContentAppliRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ContentAppliRepository extends JpaRepository<ContentAppli, String> {
-    public ContentAppli findContentAppliByContentName(String contentName);
+public interface ContentAppliRepository extends JpaRepository<ContentAppli, Integer> {
+    //public ContentAppli findContentAppliByContentName(String contentName);
 }
