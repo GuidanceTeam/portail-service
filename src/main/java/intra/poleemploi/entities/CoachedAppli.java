8dc4b02c2c0023c8620968a2c64f040a375c9ed7
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-02 15:03:10 +0200

    [Entity] CoachedAppli.java & ContentAppli.java

--- src/main/java/intra/poleemploi/entities/CoachedAppli.java
+++ src/main/java/intra/poleemploi/entities/CoachedAppli.java
@@ -6,17 +6,29 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.io.Serializable;
+import java.util.*;
 
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor @ToString
-public class CoachedAppli {
+@Data @AllArgsConstructor @NoArgsConstructor
+@Table(name="coachedAppli")
+public class CoachedAppli implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String Id;
+    private Integer id;
+    private String idCoachedAppli;
     @Column(unique = true)
     private String appliName;
-    @OneToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
+    //@OneToMany (cascade = CascadeType.ALL, mappedBy = "coachedAppli", orphanRemoval = true)
     private Collection<ContentAppli> contents = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "CoachedAppli{" +
+                "id=" + id +
+                ", idCoachedAppli='" + idCoachedAppli + '\'' +
+                ", appliName='" + appliName + '\'' +
+                '}';
+    }
 }
--- src/main/java/intra/poleemploi/entities/ContentAppli.java
+++ src/main/java/intra/poleemploi/entities/ContentAppli.java
@@ -5,23 +5,28 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor @ToString
-public class ContentAppli {
+@Table(name="contentAppli")
+public class ContentAppli implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Integer id;
+    private String idContent;
     private String contentName;
-    private boolean published;
-    private String description;
-    private Long nbLectures;
-    private Long nbUsers;
-    private double displayDuration;
-    private Date date;
+    //private boolean published;
+    //private String description;
+    //private int nbLectures;
+    //private int nbUsers;
+    //private double displayDuration;
+    //private Date date;
+    //@ManyToOne(fetch = FetchType.LAZY)//(cascade = CascadeType.PERSIST)
+    //@JoinColumn(name = "coachedAppli_id")
+    private CoachedAppli coachedAppli;
+
+
 }
