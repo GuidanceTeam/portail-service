62a521828c23f98db126bfde094f9a7e8adf94a2
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-04 12:42:45 +0200

    [security] Add Headers CORS -> JWTAuthorizationFilter.java

--- src/main/java/intra/poleemploi/security/JWTAuthorizationFilter.java
+++ src/main/java/intra/poleemploi/security/JWTAuthorizationFilter.java
@@ -1,3 +1,4 @@
+
 package intra.poleemploi.security;
 
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -11,12 +12,17 @@ import java.io.IOException;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // CORS les entêtes
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, authorization");
+        response.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
 
-        String jwt = httpServletRequest.getHeader("Authorization");
-        if(jwt == null) throw new RuntimeException("Not Authorized");
-
+//        String jwt = request.getHeader("Authorization");
+//        if(jwt == null) throw new RuntimeException("Not Authorized");
+//
         // sinon
-        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        filterChain.doFilter(request, response);
     }
 }
