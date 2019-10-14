2948b3674fe0c91400b4de3fb3f48a8775a32e08
ElvireDev <39312036+elviredev@users.noreply.github.com>
2019-10-14 17:36:12 +0200

    Create UserApp.java

---
+++ src/main/java/intra/poleemploi/entities/UserApp.java
@@ -0,0 +1,29 @@
+package intra.poleemploi.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @ToString
+@Table(name="userapp")
+public class UserApp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private boolean activated;
+    @ManyToMany//(fetch = FetchType.EAGER)
+    private Collection<RoleApp> roles = new ArrayList<>();
+    @ManyToMany
+    private Collection<Appli> applis = new ArrayList<>();
+}
