3bab02237460ae2c50274f4652250cc1fa654e30
sandrine <sandrine.rodriguez@hotmail.com>
2019-10-23 12:23:21 +0200

    Modif Collection<Appli> en Set<Appli> et modif options @ManyToMany

--- src/main/java/intra/poleemploi/entities/UserApp.java
+++ src/main/java/intra/poleemploi/entities/UserApp.java
@@ -11,6 +11,7 @@ import org.hibernate.annotations.LazyCollectionOption;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor @ToString
@@ -26,7 +27,9 @@ public class UserApp {
     private boolean activated;
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<RoleApp> roles = new ArrayList<>();
-    @ManyToMany
-    @LazyCollection(LazyCollectionOption.FALSE)
-    private Collection<Appli> applis = new ArrayList<>();
+    /*@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(name="userapp_applis", joinColumns = @JoinColumn(name="user_app_id"), inverseJoinColumns = @JoinColumn(name="applis_id"))
+    @LazyCollection(LazyCollectionOption.FALSE)*/
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Appli> applis;
 }
