14e40d7c187c4cff7dbc89a0585eaf2fc9abbb92
philippe <philippe.corrihons@pole-emploi.fr>
2019-11-08 10:39:55 +0100

    httpclient connection

---
+++ src/main/java/intra/poleemploi/utility/FirstCallBeforeLogin.java
@@ -0,0 +1,9 @@
+package intra.poleemploi.utility;
+
+public class FirstCallBeforeLogin {
+
+   // http://pr051-gfpe-3upxjf0.sip91.pole-emploi.intra:22391/know/login.jsp    //prod
+   // http://kmore-gfpe-fkqt507.sii24.pole-emploi.intra:15071/know/index.jsp    //recette
+
+
+}
--- src/main/java/intra/poleemploi/utility/LoginKnowmore.java
+++ src/main/java/intra/poleemploi/utility/LoginKnowmore.java
@@ -1,5 +1,6 @@
 package intra.poleemploi.utility;
 
 public class LoginKnowmore {
-    
+    http://pr051-gfpe-3upxjf0.sip91.pole-emploi.intra:22391/know/servlet/LoginCheck
+
 }
