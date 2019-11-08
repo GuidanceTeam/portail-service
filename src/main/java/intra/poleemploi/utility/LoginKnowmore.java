925f519f2a249f6549428e26b8607415a81291b1
philostene <gspillette@gmail.com>
2019-11-08 21:10:11 +0100

    post json fix

--- src/main/java/intra/poleemploi/utility/LoginKnowmore.java
+++ src/main/java/intra/poleemploi/utility/LoginKnowmore.java
@@ -8,6 +8,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -62,14 +63,18 @@ public class LoginKnowmore {
         httpPost.setHeader("Content-type", "application/json");
         httpPost.setHeader("Accept-Encoding", "gzip, deflate");
         httpPost.setHeader("Accept-Language","fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7");
-        httpPost.setHeader("Cookie",jsessionId);
+    //    httpPost.setHeader("Cookie",jsessionId);
 
-        List<NameValuePair> params = new ArrayList<NameValuePair>();
+//        List<NameValuePair> params = new ArrayList<NameValuePair>();
+//
+//        params.add(new BasicNameValuePair("name", "ipco2530"));
+//        params.add(new BasicNameValuePair("password", "Exchange91210"));
+//
+//        httpPost.setEntity(new UrlEncodedFormEntity(params));
 
-        params.add(new BasicNameValuePair("name", "ipco2530"));
-        params.add(new BasicNameValuePair("password", "Exchange91210"));
-
-        httpPost.setEntity(new UrlEncodedFormEntity(params));
+        String json = "{\"id\":1,\"name\":\"ipco2530\",\"password\": \"Exchange91210\"}";
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
 
         CloseableHttpResponse response = httpclient.execute(httpPost);
         int statusCode = response.getStatusLine().getStatusCode();
