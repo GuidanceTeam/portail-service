f454b7e816724ccb7a28a7868ad5d1dd688dfcc2
sand <sandrine.rodriguez@hotmail.com>
2019-11-20 11:00:30 +0100

    add properties

--- src/main/java/intra/poleemploi/entities/FeedbackEmail.java
+++ src/main/java/intra/poleemploi/entities/FeedbackEmail.java
@@ -9,14 +9,30 @@ public class FeedbackEmail {
     private String name;
 
     @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String username;
+
+    @NotNull
     @Email
     private String email;
-    //private String subject;
+
+    @NotNull
+    @Email
+    private String emailUser;
+
+    @NotNull
+    private String subject;
 
     @NotNull
     @Min(10)
     private String textMessage;
 
+    // Getters and Setters
     public String getName() {
         return name;
     }
@@ -25,12 +41,52 @@ public class FeedbackEmail {
         this.name = name;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
     public String getTextMessage() {
