9013e64f7b93584049a4923aee5ab51a77cd8386
sand <sandrine.rodriguez@hotmail.com>
2019-11-21 10:45:12 +0100

    correction mineure

--- src/main/java/intra/poleemploi/dao/ContentRepository.java
+++ src/main/java/intra/poleemploi/dao/ContentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ContentRepository extends JpaRepository<Content, Integer> {
-    //public Content findContentByContentName(String contentName);
+    Content findContentByContentName(String contentName);
 }
