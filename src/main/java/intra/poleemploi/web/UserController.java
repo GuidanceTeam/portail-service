4ff3558285cf512d2379c181690e0fa477fb97ab
sand <sandrine.rodriguez@hotmail.com>
2020-01-14 12:37:58 +0100

    modif @crossOrigin

--- src/main/java/intra/poleemploi/web/UserController.java
+++ src/main/java/intra/poleemploi/web/UserController.java
@@ -19,8 +19,8 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://guidance-stats.herokuapp.com")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://guidance-stats.herokuapp.com")
 public class UserController {
     @Autowired
     private AuthService authService;
